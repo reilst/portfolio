5086cf2ef42bb09c146b2e7dff648b0cf7f7f27c
reilst <rebirth0122@gmail.com>
2020-09-06 16:49:20 +0900

    更新できるか

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          れいるのTwitter:
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/reil_st"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          @reil_st
         </a>
       </header>
     </div>
